c37e2864fd0aa65db9ac158e75a09091e3bd5deb
olatoCode <awosolat@gmail.com>
2024-02-20 10:54:42 +0100

    Basic4 file was deleted in learn folder

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -11,20 +11,17 @@ namespace MyNamespace
             //  Basic2 obj2 = new Basic2();
             // obj2.BasicMethod();
 
-            // var p1 =  new Person("Tobi", "Awosola", new DateOnly(2024, 02,12));
-            // var p2 =  new Person("peace", "glory", new DateOnly(2000, 12,30));
+            var p1 =  new Person("Tobi", "Awosola", new DateOnly(2024, 02,12));
+            var p2 =  new Person("peace", "glory", new DateOnly(2000, 12,30));
    
-            // List<Person> people = [p1,p2];
-            // System.Console.WriteLine(people.Count());
-            // foreach (var person in people)
-            // {
-            //          Console.WriteLine($"My name is {person.first} {person.last} and I was born on {person.bd}  ");
-            // }
+            List<Person> people = [p1,p2];
+            System.Console.WriteLine(people.Count());
+            foreach (var person in people)
+            {
+                     Console.WriteLine($"My name is {person.first} {person.last} and I was born on {person.bd}  ");
+            }
           
 
-
-var account = new BankAccount("Tobi", 1000,"10112345675");
-Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
           
         }
     }
